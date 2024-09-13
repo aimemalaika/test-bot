2f4f77b62fb35a089b187a14c4bfd074eea6146d
Aime Malaika <aimemalaika1995@gmail.com>
2024-09-13 15:05:44 +0200

    ...

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
+
+app.use(bodyParser.json());
+
 const port = 3000;
 
 app.get('/webhooks', (req, res) => {

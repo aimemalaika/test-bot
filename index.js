a858dbb2088d409cb35ee833af678a0fb13f91cc
Aime Malaika <aimemalaika1995@gmail.com>
2024-09-13 14:39:37 +0200

    ...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const port = 3000;
 
@@ -29,6 +31,38 @@ app.post('/webhooks', (req, res) => {
     body.entry.forEach((entry) => {
       const webhookEvent = entry.messaging[0];
       console.log(webhookEvent);
+
+      let data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": "250787561924",
+        "type": "text",
+        "text": {
+          "preview_url": false,
+          "body": "Message from WhatsApp received"
+        }
+      });
+      
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://graph.facebook.com/v20.0/353617044511044/messages',
+        headers: { 
+          'Content-Type': 'application/json', 
+          'Authorization': 'Bearer EAAL87FVcNuYBO3wmVFHZCWQyIfYNNZAFNT0HZCqDoUNytx55hoEK3vfFCIISwV6l11k3Ci0ovmFZA1JWZCU3UyRtyM0XDKNbT1b3cCcz5BhkuPa0ZBjYL3CJOFcGUgAHXZCgPIZAuq20PMuOpMGFwPHbuI3iOpQgpkIcICN74ShujBkzcHt5ZCKHHF7sR60gjsh5zXWSWcqCZAfC8FlE3niZCCf4GxbwpXCFiVy6595l8B0c0WchuOVOwAp'
+        },
+        data : data
+      };
+      
+      axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      
+
     });
     res.status(200).send('EVENT_RECEIVED');
   } else {
